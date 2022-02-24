f69ef6b38ce3f6cfce0061db3025c4a89ea372bd
bobruk76 <vladimir.m.polyakov@gmail.com>
2022-02-25 00:13:04 +0300

    add notification

--- src/components/RealTimeUpdater.vue
+++ src/components/RealTimeUpdater.vue
@@ -1,30 +1,30 @@
 <script>
 import { mapGetters, mapActions } from 'vuex'
 
-const INTERVAL_DIALOG_MS = 2000;
+// const INTERVAL_DIALOG_MS = 2000;
 const INTERVAL_NOTIFICATIONS_MS = 50000;
 
 export default {
   mounted () {
-    this.intervalForMessages = setInterval(() => {
-      if (this.activeDialog) {
-        this.loadFreshMessages(this.activeDialog.id)
-      }
-    }, INTERVAL_DIALOG_MS)
+    // this.intervalForMessages = setInterval(() => {
+    //   if (this.activeDialog) {
+    //     this.loadFreshMessages(this.activeDialog.id)
+    //   }
+    // }, INTERVAL_DIALOG_MS)
 
     this.intervalForNotifications = setInterval(() => {
       this.apiNotifications()
     }, INTERVAL_NOTIFICATIONS_MS)
   },
-  computed: {
-    ...mapGetters('profile/dialogs', ['activeDialog']),
-  },
+  // computed: {
+  //   ...mapGetters('profile/dialogs', ['activeDialog']),
+  // },
   methods: {
-    ...mapActions('profile/dialogs', ['loadFreshMessages']),
+    // ...mapActions('profile/dialogs', ['loadFreshMessages']),
     ...mapActions('profile/notifications', ['apiNotifications']),
   },
   beforeDestroy () {
-    window.clearInterval(this.intervalForMessages);
+    // window.clearInterval(this.intervalForMessages);
     window.clearInterval(this.intervalForNotifications);
   },
   render: () => null,
